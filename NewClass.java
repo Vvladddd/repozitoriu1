4155674252246e34c36dc4e855e0689a20372281
vlad info <vlad.goldaevici@gmail.com>
2019-03-15 20:31:35 +0200

    new file

---
+++ NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Solovey
+ */
+public class NewClass {   
+    public static void main(String [] args){
+        System.out.println ("Welcome...");
+        System.out.println ("Parsm 1" + args[0]);
+        System.out.println ("Param 2" + args[1]);
+    }
+}
